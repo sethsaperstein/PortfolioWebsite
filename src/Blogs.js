254c62d6c6445317d69d160c029d49331696d6f8
Seth Saperstein <42792771+sethsaperstein@users.noreply.github.com>
2021-10-24 22:19:51 -0400

    fix load time. fix image loads. fix dates. update links. (#22)

--- src/Blogs.js
+++ src/Blogs.js
@@ -12,19 +12,19 @@ class Blogs extends React.Component {
 				</div>
 				<div className="body">
                     <Card 
-						imageUrl='https://www.lynbrooklibrary.org/wp-content/uploads/2020/06/coming-soon-neon-sign.jpg'
+						imageUrl={require('../src/Images/coming-soon.jpeg')}
 						title='Coming Soon — Training For a Marathon In 1 Month — Why You Shouldn’t and How I Did'
 						text="How hard is running a marathon? How do people typically train for something like this? Is this even possible in a month? What do I need to get?
                         These are just a few of the many questions that were running through my mind at the time."
 						link='https://medium.com/@sethsaper/coming-soon-training-for-a-marathon-in-1-month-why-you-shouldnt-and-how-i-did-12ecaf6dd048'
-						date='October 17 2021'
+						date='November 12 2021'
 					/>
                     <Card 
-						imageUrl='https://www.lynbrooklibrary.org/wp-content/uploads/2020/06/coming-soon-neon-sign.jpg'
-						title='Coming Soon — 10 Things They Won’t Tell You About Software Engineering'
+						imageUrl={require('../src/Images/coding.png')}
+						title='10 Things They Won’t Tell You About Software Engineering'
 						text='Throughout the course of this guide, I will share my experiences of the constant dilemmas software engineers face each and every day.'
-						link='https://medium.com/@sethsaper/coming-soon-10-things-they-wont-tell-you-about-software-engineering-7aeef0aa6072'
-						date='September 12 2021'
+						link='https://medium.com/@sethsaper/10-things-they-wont-tell-you-about-software-engineering-6754e28c57f4'
+						date='October 24 2021'
 					/>
 				</div>
 			</div>
